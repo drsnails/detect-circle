9752e3812b64538ec5f444eca48ea3059aa0a9bd
Stav Yaar Bar <anistu@gmail.com>
2026-03-06 18:11:40 +0200

    fixed and changed constant ratio for better corner detection

--- main.js
+++ main.js
@@ -43,6 +43,10 @@ function onDown(ev) {
     gIsMouseDown = true
     gLastPos = pos
     gLastPositions = []
+    if (ev.shiftKey) {
+        gLastPositions = drawCircle(pos, 100, 0.1)
+    }
+
 }
 
 function onMove(ev) {
@@ -58,10 +62,23 @@ function onMove(ev) {
 
 function onUp() {
     gIsMouseDown = false
+
     calculateCircleAccuracy()
 }
 
 function calculateCircleAccuracy() {
+    const { avgPos, radialScore, avgDistance } = getRadialScore()
+    const sharpnessScore = getSharpnessScore()
+    console.log('avgDistance:', avgDistance)
+    // const radialPercent = avgDistance < 35 ? (avgDistance / 35) : 0.6
+    // const radialPercent = avgDistance < 40 ? (1-avgDistance / 40) : 0.6
+    const radialPercent = 0.6
+    const finalScore = (sharpnessScore * (1 - radialPercent) + radialScore * radialPercent)
+    drawText(avgPos, finalScore.toFixed(2), 12)
+    renderScore(finalScore)
+}
+
+function getRadialScore() {
     const avgPos = gLastPositions.reduce((vecSum, pos) => {
         vecSum.x += pos.x
         vecSum.y += pos.y
@@ -72,24 +89,46 @@ function calculateCircleAccuracy() {
 
     const avgDistance = calcAvgDistanceFromCenter(avgPos)
     let variance = gLastPositions.reduce((sum, pos) => {
-        let individualDist = distance(avgPos, pos);
-        let diff = Math.abs(individualDist - avgDistance) / avgDistance;
-        return sum + diff ** 2;
-    }, 0) / gLastPositions.length;
+        const individualDist = distance(avgPos, pos)
+        const diff = Math.abs(individualDist - avgDistance) / avgDistance
+        return sum + diff
+    }, 0) / gLastPositions.length
 
-    let sensitivity = 0.6
-    console.log('variance:', variance)
-    let finalScore = Math.exp(-Math.sqrt(variance) / (sensitivity));
+    let sensitivity = 0.3
+    let radialScore = Math.exp(-variance / (sensitivity))
+    return { avgPos, radialScore, avgDistance }
+}
 
-    // drawCircle(avgPos, 5, 'black')
-    drawText(avgPos, finalScore.toFixed(2), 12)
-    renderScore(finalScore)
+function getSharpnessScore() {
+    const circleVectors = gLastPositions.map((point, idx) => {
+        const nextPoint = gLastPositions[idx + 1] || {}
+        return { x: nextPoint.x - point.x, y: nextPoint.y - point.y }
+    })
+    let minDotProduct
+    let cornerCount = 0
+    circleVectors.forEach((vector, idx) => {
+        if (idx >= circleVectors.length - 5) return
+        const nextVector = circleVectors[idx + 1] || vector
+        const dotProduct = getDotProduct(vector, nextVector)
+        if (dotProduct < 0.1) cornerCount++
+        if (minDotProduct === undefined || dotProduct < minDotProduct) {
+            minDotProduct = dotProduct
+        }
+    })
+    const cornerPercent = cornerCount / circleVectors.length
+    return cornerPercent >= 0.1 ? 0.5 : 1
+
 }
 
 function renderScore(score) {
     const elPercentDisplay = document.querySelector('.percent-display h1')
     elPercentDisplay.innerText = score ? Math.round(score * 100) + '%' : ''
 }
+
+function getDotProduct(v1, v2) {
+    return (v1.x * v2.x + v1.y * v2.y) / (vectorSize(v1) * vectorSize(v2))
+}
+
 
 function calcAvgDistanceFromCenter(avgPos) {
     const avgDistance = gLastPositions.reduce((distSum, pos) => {
@@ -120,13 +159,38 @@ function getPosPixels(pos) {
 }
 
 
-function drawCircle(pos, r, fillColor) {
-    gCtx.beginPath()
-    gCtx.arc(pos.x, pos.y, r, 0, Math.PI * 2)
-    gCtx.fillStyle = fillColor
-    gCtx.fill()
+function drawCircle(pos, r, ...rest) {
+    const points = arcPoints(pos, r, ...rest)
+    points.forEach((p, idx) => {
+        gCtx.beginPath()
+        const nextPoint = points[idx + 1] || {}
+        gCtx.moveTo(nextPoint.x, nextPoint.y)
+        gCtx.lineTo(p.x, p.y)
+        gCtx.lineCap = 'round'
+        // gCtx.strokeStyle = brush.color
+        gCtx.lineWidth = 2
+        gCtx.stroke()
+
+    })
+    return points
+    // gCtx.fillStyle = fillColor
+    // gCtx.fill()
 }
 
+
+function arcPoints(pos, r, diff = 0.2, start = 0, end = 2 * Math.PI + diff) {
+    const pts = []
+
+    for (let t = start; t <= end; t += diff) {
+
+        pts.push({
+            x: pos.x + r * Math.cos(t),
+            y: pos.y + r * Math.sin(t)
+        })
+    }
+
+    return pts
+}
 
 
 function drawText(pos, text, fontSize = 16) {
@@ -135,6 +199,11 @@ function drawText(pos, text, fontSize = 16) {
     gCtx.textAlign = 'center'
     gCtx.textBaseline = 'middle'
     gCtx.fillText(text, pos.x, pos.y)
+}
+
+
+function vectorSize(v) {
+    return Math.sqrt(v.x ** 2 + v.y ** 2) || 0.1
 }
 
 
